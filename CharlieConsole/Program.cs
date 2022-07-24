05f6e2220fc7dc14cdca348b4ef8b7d398c52f13
procharlie8 <charlienewman2507@gmail.com>
2022-07-24 16:03:29 +0100

    console app started

---
+++ CharlieConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿// Variable - a place to store a value
+
+// Method - a place where code exists and is stored
+// void - a method that does not return anything - just runs some code
+
+// Class - an object to store Variables and Methods (also other Classes)
+
+// types of variables
+string birthdayText = "charlie becoming cheese";
+
+// This is a method called "WriteLine" that is part of the the class "Console"
+Console.WriteLine(birthdayText);
